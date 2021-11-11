1758feefece76ddc10e5205a37c8fd245bda05c9
Anderson Pereira <anderson.carro.95@gmail.com>
2021-11-11 01:24:09 -0300

    Chapter I - Utilizando source maps

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
 export function App() {
+  throw new Error(
+    "Nessa passo melhoramos como é exibido o erro de uma forma que podemos identificar mais facilmente no console."
+  );
   return <h1>Hello World</h1>;
 }
