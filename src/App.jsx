10fed41b6da74eb2a323e3eac95a5e4ab5ff11a8
Anderson Pereira <anderson.carro.95@gmail.com>
2021-11-14 00:06:02 -0300

    Chapter I - Importando arquivos CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
+import "./styles/global.css";
+
 export function App() {
-  throw new Error(
-    "Nessa passo melhoramos como é exibido o erro de uma forma que podemos identificar mais facilmente no console."
-  );
   return <h1>Hello World</h1>;
 }
